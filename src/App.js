2f713e528ee8e7999180d4cd036e7afb4183fb22
VaneSanjinez <vane.sanji@gmail.com>
2021-05-02 19:05:49 -0400

    #001 Start communication with BE using fetch from react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import ListOfProjects from './components/ListOfProjects';
 import './services/GitlabServices'
 import getGitlabUrl, { getReportHello, sum } from './services/GitlabServices';
 
 function App() {
+  // const [projects, setProjects] = useState([])
+  // useEffect(function(){
+  //   console.log('effect ran')
+  //   fetch('http://localhost:9091/gitlab/projects/')
+  //     .then(res =>res.json())
+  //     .then(response =>{
+  //       const {data} = response
+  //       console.log("^^^^^^^^^^^^^^" + {data} + "^^^^^^^^^^^^")
+  //       const projects = data.map(project => project.id)
+  //       console.log(projects)
+  //     })
+  //   }, [])
+
+  // useEffect(() => {
+  //   fetch('http://localhost:9091/gitlab/projects/')
+  //   .then((res) => res.json())
+  //   .then((projects) => {
+  //     setProjects(projects);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err)
+  //   }, [])
+  // })
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        {getGitlabUrl()}
-        {console.log(sum(100,5))}
-        {console.log(getReportHello())}
-        <p>
+        {/* {getGitlabUrl()} */}
+        {/* {console.log(sum(100,5))} */}
+        {/* {console.log(getReportHello())} */}
+        <div>
           <p>Report generator!</p>
-        </p>
+          {/* {projects.map((singleProject) => (<div>{singleProject.id}</div>))} */}
+          <ListOfProjects className="listProjects">
+            projects!
+          </ListOfProjects>
+{/* 
+          {
+            projects.map(singleProject => <div>{singleProject}</div>)
+          } */}
+        </div>
       </header>
     </div>
   );
--- src/services/GitlabServices.js
+++ src/services/GitlabServices.js
@@ -1,10 +1,8 @@
-const port = 9091;
-const baseUrl = 'http://localhost:' + port + '/';
+const reportRegeratorPort = 9091;
+const baseUrl = 'http://localhost:' + reportRegeratorPort + '/';
 
 export default function getGitlabUrl() {
-    console.log("*********************")
     console.log(baseUrl)
-    console.log("*********************")
 } 
 
 export function sum(a, b){
@@ -13,10 +11,36 @@ export function sum(a, b){
 
 export function getReportHello(){
     const helloUrl = baseUrl + 'hello';
-    console.log(helloUrl)
-    return  fetch(helloUrl)
+    return fetch(helloUrl)
     .then(res => res.text())
     .then(response => {
         console.log(response)
     })
+}
+// http://localhost:9091/gitlab/projects/
+export function getAllProjects(){
+    const url = baseUrl + '/gitlab/projects/'
+    console.log(url)
+    return fetch(url)
+           .then(res => res.json())
+           .then(response => {
+            //    var {data} = response
+               console.log(response.length);
+            //    if(response.length > 0){
+            //        console.log("length is higher than 0")
+                   const data = response
+            //    }
+               
+            //    const {data=[]} = response
+               console.log({data})
+            //    console.log({data})
+                if(Array.isArray(data)){
+                    const projects = data.map(project => {
+                        const{id, description, name, webUrl} = project
+                        return {id, description, name, webUrl}
+                    })
+                    return projects;
+                }
+           })
+
 }
