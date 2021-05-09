b88d84c51943df98a81c4553e0b369b684cdf1b9
VaneSanjinez <vane.sanji@gmail.com>
2021-05-08 23:24:15 -0400

    004 #Install wouter. Add function to get specific project from URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ListOfProjects from './components/ListOfProjects';
 import Project, { ProjectById } from './components/Project';
 import './services/GitlabServices'
 import getGitlabUrl, { getProjectById, getReportHello, sum } from './services/GitlabServices';
+import {Route} from 'wouter';
 
 function App() {
   return (
@@ -12,10 +13,14 @@ function App() {
         {getGitlabUrl()}
         <div>
           <p>Report generator!</p>
+          {/* <Route path='/rp/:id' component = {ProjectById}></Route> */}
+          <Route path="/rp/:id">
+            {params => <ProjectById id={params.id} />}
+          </Route>
           {/* <ListOfProjects className="listProjects"/> */}
         </div>
         <div>
-          <ProjectById id={18625237}/>
+          {/* <ProjectById id={18625237}/> */}
         </div>
       </header>
     </div>
