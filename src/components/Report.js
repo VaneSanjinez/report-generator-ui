470440cf4c3f4d475933801c182a30152e43664e
VaneSanjinez <vane.sanji@gmail.com>
2021-06-20 19:13:46 -0400

    015 # Return component instead of function for report details and move map to Report object instead of call

--- src/components/Report.js
+++ src/components/Report.js
@@ -4,38 +4,64 @@ import ReportInfo from './ReportInfo';
 
 
 export default function Report({currentDate, projectName, projectUrl, projectMember, reportDetails}){
+   
     return ( <div>
         <h4>{currentDate}</h4>
         <h5>{projectName}</h5>
         <h5>{projectUrl}</h5>
-        <h5>{projectMember}</h5>
-        <h5>{reportDetails}</h5>
-    </div>)
-    // https://www.robinwieruch.de/react-list-component
-}
-
-export function GitlabReport({projectId, authorEmail}){
-    console.log('after entering gitlab method before define const')
-    const [report, setReport] = useState([]);
-
-    useEffect(function(){
-        getGitlabReportByProjectIdAndUserEmail(projectId, authorEmail)
-        .then(report => setReport(report))
-    }, []);
-    return <div>
-            <Report
-                currentDate = {report.currentDate}
-                projectName = {report.projectName}
-                projectUrl = {report.projectUrl}
-                reportDetails = {report.reportDetails.map(({authorName, commitDate, details, hours, percent}) => 
-                        <ReportInfo 
+        <h1>{projectMember}</h1>
+        {/* {reportDetails.map(({authorName, commitDate, details, hours, percent}) => 
+                    <ReportInfo 
                             authorName = {authorName}
                             commitDate = {commitDate}
                             details = {details}
                             hours = {hours}
                             percent = {percent} />
-                )}
-            />
-    </div>
+                )} */}
+    </div>)
+    // https://www.robinwieruch.de/react-list-component
 }
 
+export function GitlabReport({projectId, authorEmail}){
+    console.log('after entering gitlab method before define const');
+    const [loading, setLoading] = useState(false);
+    const [report, setReport] = useState([]);
+    console.log('before use effect');
+    useEffect(function(){
+        setLoading(true);
+        setTimeout(() => {
+            getGitlabReportByProjectIdAndUserEmail(projectId, authorEmail)
+            .then(report => setReport(report))
+            .finally(() => {
+                setLoading(false)
+            })
+        }, 10);
+       
+    }, []);
+    console.log('after use effect');
+    if (loading) {
+        return <p>Report data is loading...</p>;
+    }
+
+    if (Array.isArray(report.reportDetails) && report.reportDetails.length > 0) {
+        console.log("1111111111111111111111122222222222222222233333333333333333")
+        
+    }
+    return <div>
+            <Report
+                currentDate = {report.currentDate}
+                projectName = {report.projectName}
+                projectUrl = {report.projectUrl}
+                reportDetails={report.reportDetails} 
+                // reportDetails={report.reportDetails.map(({authorName, commitDate, details, hours, percent}) => 
+                //     <ReportInfo 
+                //             authorName = {authorName}
+                //             commitDate = {commitDate}
+                //             details = {details}
+                //             hours = {hours}
+                //             percent = {percent} />
+                // )}
+            />
+    </div>
+
+}
--- src/components/ReportInfo.js
+++ src/components/ReportInfo.js
@@ -1,11 +1,29 @@
-import React from 'react';
+// import React from 'react';
 
-export default function ReportInfo({authorName, commitDate, details, hours, percent}){
-    return ( <div>
-        <h4>{authorName}</h4>
-        <h5>{commitDate}</h5>
-        <span>{details}</span>
-        <h5>{hours}</h5>
-        <h5>{percent}</h5>
-    </div>)
-}
+// export default function ReportInfo({authorName, commitDate, details, hours, percent}){
+//     return ( <div>
+//         <h4>{authorName}</h4>
+//         <h5>{commitDate}</h5>
+//         <span>{details}</span>
+//         <h5>{hours}</h5>
+//         <h5>{percent}</h5>
+//     </div>)
+// }
+
+import React, { Component } from 'react';
+
+class ReportInfo extends Component {
+    
+    render() {
+      return ( <div>
+        <h4>{this.props.authorName}</h4>
+        <h5>{this.props.commitDate}</h5>
+        <span>{this.props.details}</span>
+        <h5>{this.props.hours}</h5>
+        <h5>{this.props.percent}</h5>
+    </div>
+      );
+    }
+  }
+   
+  export default ReportInfo;
