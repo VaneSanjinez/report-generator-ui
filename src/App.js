c610b95befe38c8bc835ec74d9c04a0cbe98ee24
VaneSanjinez <vane.sanji@gmail.com>
2021-07-20 22:05:47 -0400

    016 #Refactor code add new components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import './App.css';
-import ListOfProjects, { ListOfProjectsByUserId } from './components/ListOfProjects';
-import Project, { ProjectById } from './components/Project';
 import './services/GitlabServices'
-import ListOfMembers from './components/ListOfMembers';
-import { getCommitsByProjectId, getCommitsSinceDate, getGitlabReportByProjectIdAndUserEmail, getProjectBranches } from './services/GitlabServices';
-import ListOfCommits, { ListOfCommitsSinceDate, ListOfCommitsSinceUntilDates, ListOfCommitsUntilDate } from './components/ListOfCommits';
-import { CommitById } from './components/Commit';
-import ListOfBranches from './components/ListOfBranches';
-import { BranchById } from './components/Branch';
-import {GitlabReportInfo, GitlabReportDetails} from './components/Report';
-import {UserByEmail} from './components/User';
 import createRoutes from './utils/Routes';
+import Home from './components/Home';
+import ReportComponent from './components/ReportComponent';
 
 function App() {
   const routes = createRoutes();
@@ -18,12 +10,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div>
-          <p>Report generator</p>
+          <Home/>
           {routes}
-        </div>
-        <div>
-          <GitlabReportInfo projectId ={250833} authorEmail = {'sazzopardi@gitlab.com'}/>
-          <GitlabReportDetails projectId ={250833} authorEmail = {'sazzopardi@gitlab.com'}/>
+          <ReportComponent/>
         </div>
       </header>
     </div>
